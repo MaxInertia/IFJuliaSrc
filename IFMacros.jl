05ed4cc86f20fbbb52c83853f5c18ef5e0df8960
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-05-04 14:18:13 -0500

    Add @Struct for concise creation of single value structures

---
+++ IFMacros.jl
@@ -0,0 +1,16 @@
+module IFMacros
+
+"""
+Macro to concisely define a struct with a single field.
+Example Usage:
+    `@defineStruct Quantity{T <: Number} value T`
+"""
+macro Struct(structName, field, fieldType)
+    return :(
+        struct $structName
+            $field::$fieldType
+        end
+    )
+end
+
+end #IFMacros
