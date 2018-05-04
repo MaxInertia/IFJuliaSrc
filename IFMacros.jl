e1c39bbf325ee55a1ffa5c0c927ae2aa5081fe1d
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-05-04 16:49:28 -0500

    Combine arguments for field-name and field-type

--- IFMacros.jl
+++ IFMacros.jl
@@ -5,13 +5,14 @@ export @Struct
 """
 Concisely define a struct with a single field.
 Example Usage:
-    - `@Struct Quantity value Number`
-    - `@Struct(Quantity, value, Number)`
+    - `@Struct Quantity value::Number`
+    - `@Struct(Quantity, value::Number)`
+    - `@Struct(Quantity{T}, value: T)`
 """
-macro Struct(structName, field, fieldType)
+macro Struct(structName, fieldWithType)
     return :(
         struct $structName
-            $field::$fieldType
+            $fieldWithType
         end
     )
 end
@@ -19,14 +20,15 @@ end
 """
 Concisely define a struct with two fields.
 Example Usage:
-    - `@Struct Quantity value Number unit Any`
-    - `@Struct(Quantity, value, Number, unit, Any)`
+    - `@Struct Quantity value::Number unit::Any`
+    - `@Struct(Quantity, value::Number, unit::Any)`
+    - `@Struct(Quantity{T}, value: T, unit::Any)`
 """
-macro Struct(structName, field1Name, field1Type, field2Name, field2Type)
+macro Struct(structName, field1, field2)
     return :(
         struct $structName
-            $field1Name::$field1Type
-            $field2Name::$field2Type
+            $field1
+            $field2
         end
     )
 end
